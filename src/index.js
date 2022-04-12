9bdac545d7ad32718b45bca89555baab8e61c1d9
matiasangelani <matiasangelani2@gmail.com>
2022-04-12 18:20:49 -0300

    index wrap with Provider

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
-//import store from './redux/store/store';
+import store from './redux/store';
 
 import App from './App';
 
 ReactDOM.render(
-  //<Provider store={store}>
-  <Router>
-    <App />
-  </Router>,
-  //</Provider>
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
