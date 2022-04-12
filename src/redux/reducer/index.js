4b49fff8e758519b11af39f22b6a74ae8c94588e
matiasangelani <matiasangelani2@gmail.com>
2022-04-12 18:26:09 -0300

    initialState caretakerProfile and case GET_CARETAKER_DETAILS

---
+++ src/redux/reducer/index.js
@@ -0,0 +1,31 @@
+import { GET_CARETAKER_DETAILS } from '../actions';
+
+const initialState = {
+  caretakerProfile: {},
+};
+
+const caretakerReducer = (state = initialState, action) => {
+  const { type, payload } = action;
+  switch (type) {
+    // case GET_POKEMONS:
+    // 	let takeOutPrevDb = state.pokemons.filter(
+    // 		(pokemon) => pokemon.createdDb === false
+    // 	);
+
+    // 	return {
+    // 		...state,
+    // 		pokemons: [...takeOutPrevDb, ...payload],
+    // 		filteredPokemons: [...takeOutPrevDb, ...payload],
+    // 	};
+    case GET_CARETAKER_DETAILS:
+      return {
+        ...state,
+        caretakerProfile: payload,
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default caretakerReducer;
