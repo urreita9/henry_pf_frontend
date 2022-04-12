af68fafbe3c0a4efc5508568d0941bee83fee2b7
matiasangelani <matiasangelani2@gmail.com>
2022-04-12 18:27:53 -0300

    component for only caretaker description

---
+++ src/components/CaretakerDescription/index.jsx
@@ -0,0 +1,38 @@
+import { Rating } from '@mui/material';
+
+const CaretakerDescription = ({ name, img, description, rating }) => {
+  return (
+    <>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          alignItems: 'center',
+        }}
+      >
+        <img
+          src={img}
+          alt='img'
+          style={{
+            width: 200,
+            height: 200,
+          }}
+        />
+        <div>
+          <p>{name}</p>
+          <p>{description}</p>
+        </div>
+      </div>
+      <div>
+        <Rating
+          name='half-rating-read'
+          value={rating || 0}
+          precision={0.5}
+          readOnly
+        />
+      </div>
+    </>
+  );
+};
+
+export default CaretakerDescription;
