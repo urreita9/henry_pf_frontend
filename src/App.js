5efd7d69d5cf921035ba50acbdf8ae1c1dac9778
urreita9 <francisco.urrea9@gmail.com>
2022-04-13 16:44:06 -0300

    Map arreglado /home

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
 	return (
 		<Router>
 			<Routes>
-				<Route exact path='/home' element={<Home />} />
+				<Route exact path='/' element={<Home />} />
 				<Route exact path='/caretaker/:id' element={<Detail />} />
 			</Routes>
 		</Router>
