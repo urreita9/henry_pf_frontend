4df3493449ee9e14cb68aaf70608806c774961b8
Mariano <mariano.bustos.moron@gmail.com>
2022-04-13 16:37:58 -0300

    UPDATED NAVBAR

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import NavBar from "./NavBar/NavBar";
-
+import { useAuth0 } from "@auth0/auth0-react";
+import { Profile } from "./components/Profile/Profile";
 const App = () => {
+  const { isAuthenticated } = useAuth0();
   return (
     <>
       <NavBar />
+      {isAuthenticated && <Profile />}
     </>
   );
 };
