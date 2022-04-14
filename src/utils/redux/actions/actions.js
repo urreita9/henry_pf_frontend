3ce704af4ae65b92883831eadcb0198d4cd8e5f0
cicarelligiuliano <cicarelligiuliano@outlook.com.ar>
2022-04-14 02:45:04 -0300

    Prueba 2

---
+++ src/utils/redux/actions/actions.js
@@ -0,0 +1,37 @@
+import axios from 'axios';
+export const GET_CARETAKER_DETAILS = 'GET_CARETAKER_DETAILS';
+export const POST_CARETAKER_QUESTION = 'POST_CARETAKER_QUESTION';
+export const FILTER_BY_PET = 'FILTER_BY_PET';
+
+export const getCaretakerDetails = (id) => async (dispatch) => {
+  try {
+    const { data } = await axios.get(`http://localhost:3001/caretaker/${id}`);
+
+    dispatch({
+      type: GET_CARETAKER_DETAILS,
+      payload: data,
+    });
+  } catch (error) {
+    alert(error);
+  }
+};
+
+export const postCaretakerQuestion = (id, question) => async (dispatch) => {
+  try {
+    const { data } = await axios.post(`http://localhost:3001/caretaker/${id}`, {
+      question,
+    });
+
+    dispatch({
+      type: POST_CARETAKER_QUESTION,
+      payload: data,
+    });
+  } catch (error) {
+    alert(error);
+  }
+};
+
+export const filterByPetSize = (payload) => ({
+  type: FILTER_BY_PET,
+  payload,
+});
