be0319c94857d5b106a596dfc69d893788c41a8a
Mariano <mariano.bustos.moron@gmail.com>
2022-04-13 23:53:51 -0300

    navbar style hotfix

--- src/components/Logout/Logout.jsx
+++ src/components/Logout/Logout.jsx
@@ -1,15 +1,16 @@
 import { useAuth0 } from "@auth0/auth0-react";
+import Button from "@mui/material/Button";
 
 export const LogoutButton = () => {
   const { logout } = useAuth0();
 
   return (
-    <button
-      className="btn btn-danger
-    "
+    <Button
+      variant="outlined"
+      color="error"
       onClick={() => logout({ returnTo: window.location.origin })}
     >
       Logout
-    </button>
+    </Button>
   );
 };
