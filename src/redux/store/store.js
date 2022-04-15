30d4bf285ab6a6ea12b5706ee5f305681a6da148
matiasangelani <matiasangelani2@gmail.com>
2022-04-15 02:25:14 -0300

    import reducers changed to reducer

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from '@redux-devtools/extension';
-import cuidadoresReducer from '../reducers/cuidadoresReducer';
+import cuidadoresReducer from '../reducer/cuidadoresReducer';
 
 const store = createStore(
   cuidadoresReducer,
