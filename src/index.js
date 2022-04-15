2b0b88016a557b34e377b99e80e992660659ef20
Augusto Blanco <blanco.augusto.r@gmail.com>
2022-04-15 19:15:17 -0300

    Add assets/mui-theme-config/theme-mui.js, and add themeProvider from index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
+import { ThemeProvider } from '@mui/material/styles'
+import theme from './assets/mui-theme-config/theme-mui'
 import { Auth0Provider } from "@auth0/auth0-react";
 import App from "./App";
 import store from "./redux/store/store";
 
 ReactDOM.render(
   <Provider store={store}>
+      <ThemeProvider theme={theme}>
+
     <Auth0Provider
       domain="dev-oe7eehce.us.auth0.com"
       clientId="XlmeWbwAL1Y0aSVChDnZlOL6XtzYYv63"
@@ -14,6 +18,7 @@ ReactDOM.render(
     >
       <App />
     </Auth0Provider>
+      </ThemeProvider>
   </Provider>,
   document.getElementById("root")
 );
