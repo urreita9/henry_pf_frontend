6654aab82a892ae2bc8c3de00b42cc81f72a505f
matiasangelani <matiasangelani2@gmail.com>
2022-04-15 02:30:27 -0300

    CRUD caretaker actions added to actions

--- src/redux/actions/actions.js
+++ src/redux/actions/actions.js
@@ -1,13 +1,29 @@
-import axios from 'axios';
+import api from '../../axios';
+
+export const GET_CARETAKERS = 'GET_CARETAKERS';
 export const GET_CARETAKER_DETAILS = 'GET_CARETAKER_DETAILS';
+export const POST_CARETAKER = 'POST_CARETAKER';
 export const POST_CARETAKER_QUESTION = 'POST_CARETAKER_QUESTION';
+export const EDIT_CARETAKER = 'EDIT_CARETAKER';
+export const DELETE_CARETAKER = 'DELETE_CARETAKER';
 export const FILTER_BY_PET = 'FILTER_BY_PET';
+
+export const getCaretakers = () => async (dispatch) => {
+  try {
+    const { data } = await api.get(`/caretakers`);
+
+    dispatch({
+      type: GET_CARETAKERS,
+      payload: data,
+    });
+  } catch (error) {
+    alert(error);
+  }
+};
 
 export const getCaretakerDetails = (id) => async (dispatch) => {
   try {
-    const { data } = await axios.get(
-      `http://localhost:3001/api/caretakers/${id}`
-    );
+    const { data } = await api.get(`/caretakers/${id}`);
 
     dispatch({
       type: GET_CARETAKER_DETAILS,
@@ -18,17 +34,55 @@ export const getCaretakerDetails = (id) => async (dispatch) => {
   }
 };
 
+export const postCaretaker = () => async (dispatch) => {
+  try {
+    const { data } = await api.post(`/caretakers`);
+
+    dispatch({
+      type: POST_CARETAKER,
+      payload: data,
+    });
+  } catch (error) {
+    alert(error);
+  }
+};
+
 export const postCaretakerQuestion = (id, question) => async (dispatch) => {
   try {
-    const { data } = await axios.post(
-      `http://localhost:3001/api/caretakers/question/${id}`,
-      {
-        question,
-      }
-    );
+    const { data } = await api.post(`/caretakers/question/${id}`, {
+      question,
+    });
 
     dispatch({
       type: POST_CARETAKER_QUESTION,
+      payload: data,
+    });
+  } catch (error) {
+    alert(error);
+  }
+};
+
+export const editCaretaker = (id, caretaker) => async (dispatch) => {
+  try {
+    const { data } = await api.put(`/caretakers/${id}`, {
+      caretaker,
+    });
+
+    dispatch({
+      type: EDIT_CARETAKER,
+      payload: data,
+    });
+  } catch (error) {
+    alert(error);
+  }
+};
+
+export const deleteCaretaker = (id) => async (dispatch) => {
+  try {
+    const { data } = await api.delete(`/caretakers/${id}`);
+
+    dispatch({
+      type: DELETE_CARETAKER,
       payload: data,
     });
   } catch (error) {
