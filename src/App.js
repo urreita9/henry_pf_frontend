042b3ba0ab00eec03e060831357d372dad69da79
matiasangelani <matiasangelani2@gmail.com>
2022-04-15 21:00:43 -0300

    route /test/cloudinary added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import CaretakerProfile from './components/CaretakerProfile';
 import NavBar from './components/NavBar/NavBar';
 import { Home } from './components/Home/Home.jsx';
+import TestCloudinary from './components/TestCloudinary';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route exact path='/' element={<Home />} />
           <Route exact path='/caretaker/:id' element={<CaretakerProfile />} />
           {/* <Route exact path='/caretaker/:id' element={<Detail />} /> */}
+          <Route exact path='/test/cloudinary' element={<TestCloudinary />} />
         </Routes>
       </Router>
     </>
