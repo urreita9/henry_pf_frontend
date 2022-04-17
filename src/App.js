2448101035be9a66f8d7da9c6e80d716a50b5e5d
Mariano <mariano.bustos.moron@gmail.com>
2022-04-17 18:12:24 -0300

    agregada ruta de usuario y tarjeta mockeada

--- src/App.js
+++ src/App.js
@@ -1,46 +1,48 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import CaretakerProfile from './views/CaretakerProfile/index';
-import NavBar from './components/NavBar/NavBar';
-import { Mapa } from './views/Map/Mapa';
-import { Home } from './views/Home/Home';
-import RegisterForm from './views/RegisterForm/RegisterForm';
-import { LoginForm } from './views/LoginForm/LoginForm';
-import { CuidadorForm } from './views/Forms/CuidadorForm';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import modeThemePalette from './assets/mui-theme-config/theme-mui';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import CaretakerProfile from "./views/CaretakerProfile/index";
+import NavBar from "./components/NavBar/NavBar";
+import { Mapa } from "./views/Map/Mapa";
+import { Home } from "./views/Home/Home";
+import RegisterForm from "./views/RegisterForm/RegisterForm";
+import { LoginForm } from "./views/LoginForm/LoginForm";
+import { CuidadorForm } from "./views/Forms/CuidadorForm";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import modeThemePalette from "./assets/mui-theme-config/theme-mui";
+import Profile from "./components/Profile/Profile";
 
 function App() {
-	const [mode, setMode] = useState('light');
+  const [mode, setMode] = useState("light");
 
-	useEffect(() => {}, [mode]);
+  useEffect(() => {}, [mode]);
 
-	const theme = React.useMemo(
-		() => createTheme(modeThemePalette(mode)),
-		[mode]
-	);
+  const theme = React.useMemo(
+    () => createTheme(modeThemePalette(mode)),
+    [mode]
+  );
 
-	function onToggleThemeMode(typeMode) {
-		console.log(typeMode);
-		typeMode ? setMode('dark') : setMode('light');
-	}
+  function onToggleThemeMode(typeMode) {
+    console.log(typeMode);
+    typeMode ? setMode("dark") : setMode("light");
+  }
 
-	return (
-		<>
-			<ThemeProvider theme={theme}>
-				<Router>
-					<NavBar onToggle={onToggleThemeMode} typeMode={mode} />
-					<Routes>
-						<Route exact path='/' element={<Home />} />
-						<Route exact path='/map' element={<Mapa />} />
-						<Route exact path='/register' element={<RegisterForm />} />
-						<Route exact path='/login' element={<LoginForm />} />
-						<Route exact path='/caretaker/:id' element={<CaretakerProfile />} />
-						<Route exact path='/host' element={<CuidadorForm />} />
-					</Routes>
-				</Router>
-			</ThemeProvider>
-		</>
-	);
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <Router>
+          <NavBar onToggle={onToggleThemeMode} typeMode={mode} />
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/map" element={<Mapa />} />
+            <Route exact path="/register" element={<RegisterForm />} />
+            <Route exact path="/login" element={<LoginForm />} />
+            <Route exact path="/caretaker/:id" element={<CaretakerProfile />} />
+            <Route exact path="/host" element={<CuidadorForm />} />
+            <Route exact path="/profile" element={<Profile />} />
+          </Routes>
+        </Router>
+      </ThemeProvider>
+    </>
+  );
 }
 export default App;
