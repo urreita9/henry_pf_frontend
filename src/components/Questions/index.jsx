ae891221f9528af7d33ca790143a93cc7d9813c2
matiasangelani <matiasangelani2@gmail.com>
2022-04-18 18:57:10 -0300

    verification map questions added

--- src/components/Questions/index.jsx
+++ src/components/Questions/index.jsx
@@ -8,99 +8,99 @@ import QuestionMarkOutlinedIcon from '@mui/icons-material/QuestionMarkOutlined';
 import AddIcon from '@mui/icons-material/AddCircleOutlineOutlined';
 
 const Questions = ({ questions }) => {
-	const { id } = useParams();
+  const { id } = useParams();
 
-	const dispatch = useDispatch();
-	const [isAdding, setIsAdding] = useState(false);
-	const [inputQuestion, setInputQuestion] = useState({
-		question: '',
-	});
-	const { logged } = useSelector((state) => state.userReducer);
-	const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [isAdding, setIsAdding] = useState(false);
+  const [inputQuestion, setInputQuestion] = useState({
+    question: '',
+  });
+  const { logged } = useSelector((state) => state.userReducer);
+  const navigate = useNavigate();
 
-	const handleOnChange = (e) => {
-		//console.log(input);
-		setInputQuestion({ ...inputQuestion, question: e.target.value });
-	};
+  const handleOnChange = (e) => {
+    //console.log(input);
+    setInputQuestion({ ...inputQuestion, question: e.target.value });
+  };
 
-	const handleOnClick = (e) => {
-		//llamar a action para crear estado nuevo en reducer
-		dispatch(postCaretakerQuestion(id, inputQuestion.question));
-		//console.log(input.question);
-		setInputQuestion({ question: '' });
-	};
+  const handleOnClick = (e) => {
+    //llamar a action para crear estado nuevo en reducer
+    dispatch(postCaretakerQuestion(id, inputQuestion.question));
+    //console.log(input.question);
+    setInputQuestion({ question: '' });
+  };
 
-	return (
-		<Grid
-			item
-			xs={12}
-			sx={{ marginBottom: 2, paddingX: 2, margin: '10px auto' }}
-		>
-			{isAdding ? (
-				<>
-					<TextField
-						fullWidth
-						sx={{ marginTop: 2, marginBottom: 1, borderColor: '#F29278' }}
-						placeholder='New Question'
-						autoFocus
-						multiline
-						label='New Question'
-						helperText={
-							!inputQuestion.length && 'Ask anything you like to your candidate'
-						}
-						// error={!inputQuestion.length && isTouched}
-						value={inputQuestion.question}
-						onChange={handleOnChange}
-						rows={4}
-						// onBlur={() => setIsTouched(true)}
-					/>
-					<Box display='flex' justifyContent='space-between'>
-						<Button
-							variant='text'
-							onClick={() => {
-								setIsAdding(false);
-								setInputQuestion({ ...inputQuestion, question: '' });
-								// setIsTouched(false);
-							}}
-						>
-							Cancel
-						</Button>
-						<Button
-							variant='outlined'
-							color='secondary'
-							// endIcon={<QuestionMarkOutlinedIcon />}
-							onClick={() => {
-								if (!logged) {
-									navigate('/login');
-									return;
-								}
-								handleOnClick();
-							}}
-							sx={{
-								borderColor: '#F29278',
-								color: '#F29278',
-							}}
-						>
-							ASK
-							{/* <QuestionMarkOutlinedIcon sx={{ fontSize: 'medium' }} /> */}
-						</Button>
-					</Box>
-				</>
-			) : (
-				<Button
-					startIcon={<AddIcon />}
-					fullWidth
-					variant='outlined'
-					sx={{
-						borderColor: '#F29278',
-						color: '#F29278',
-					}}
-					onClick={() => setIsAdding(true)}
-				>
-					Ask a question to the caretaker!
-				</Button>
-			)}
-			{/* <div>
+  return (
+    <Grid
+      item
+      xs={12}
+      sx={{ marginBottom: 2, paddingX: 2, margin: '10px auto' }}
+    >
+      {isAdding ? (
+        <>
+          <TextField
+            fullWidth
+            sx={{ marginTop: 2, marginBottom: 1, borderColor: '#F29278' }}
+            placeholder='New Question'
+            autoFocus
+            multiline
+            label='New Question'
+            helperText={
+              !inputQuestion.length && 'Ask anything you like to your candidate'
+            }
+            // error={!inputQuestion.length && isTouched}
+            value={inputQuestion.question}
+            onChange={handleOnChange}
+            rows={4}
+            // onBlur={() => setIsTouched(true)}
+          />
+          <Box display='flex' justifyContent='space-between'>
+            <Button
+              variant='text'
+              onClick={() => {
+                setIsAdding(false);
+                setInputQuestion({ ...inputQuestion, question: '' });
+                // setIsTouched(false);
+              }}
+            >
+              Cancel
+            </Button>
+            <Button
+              variant='outlined'
+              color='secondary'
+              // endIcon={<QuestionMarkOutlinedIcon />}
+              onClick={() => {
+                if (!logged) {
+                  navigate('/login');
+                  return;
+                }
+                handleOnClick();
+              }}
+              sx={{
+                borderColor: '#F29278',
+                color: '#F29278',
+              }}
+            >
+              ASK
+              {/* <QuestionMarkOutlinedIcon sx={{ fontSize: 'medium' }} /> */}
+            </Button>
+          </Box>
+        </>
+      ) : (
+        <Button
+          startIcon={<AddIcon />}
+          fullWidth
+          variant='outlined'
+          sx={{
+            borderColor: '#F29278',
+            color: '#F29278',
+          }}
+          onClick={() => setIsAdding(true)}
+        >
+          Ask a question to the caretaker!
+        </Button>
+      )}
+      {/* <div>
 				<textarea
 					name='question'
 					id='question'
@@ -113,12 +113,12 @@ const Questions = ({ questions }) => {
 				<button onClick={handleOnClick}>Preguntar</button>
 			</div>
 			<div> */}
-			{questions?.map((r) => (
-				<Question key={r.id} {...r} />
-			))}
-			{/* </div>  */}
-		</Grid>
-	);
+      {questions?.map((r) => (
+        <Question key={r.id} {...r} />
+      ))}
+      {/* </div>  */}
+    </Grid>
+  );
 };
 
 export default Questions;
