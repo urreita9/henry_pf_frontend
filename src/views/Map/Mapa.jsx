1a4cab5dd93ad3de0122e5e2abf87663a9569dea
urreita9 <francisco.urrea9@gmail.com>
2022-04-18 23:42:46 -0300

    deploy 5

--- src/views/Map/Mapa.jsx
+++ src/views/Map/Mapa.jsx
@@ -9,7 +9,13 @@ import Map, {
 import { useDispatch, useSelector } from 'react-redux';
 import { cleanCaretaker, getCaretakers } from '../../redux/actions/actions';
 import PopUpData from './PopUpData';
+
+import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
+// @ts-ignore
+// eslint-disable-next-line import/no-webpack-loader-syntax, import/no-unresolved
+mapboxgl.workerClass =
+	require('worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker').default;
 
 const initialPoint = {
 	longitude: -58.381592,
