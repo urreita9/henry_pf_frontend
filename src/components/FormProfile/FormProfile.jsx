3d6b97fdf1778ebc220dad258b1b712f157eed44
Mariano <mariano.bustos.moron@gmail.com>
2022-04-22 18:20:09 -0300

    added swal for pet

--- src/components/FormProfile/FormProfile.jsx
+++ src/components/FormProfile/FormProfile.jsx
@@ -1,170 +1,196 @@
-import Visibility from '@mui/icons-material/Visibility';
-import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
 import {
-    Box,
-    Button,
-    FilledInput,
-    FormControl,
-    IconButton,
-    InputAdornment,
-    InputLabel,
-    TextField,
-} from '@mui/material';
-import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { editUser } from '../../redux/actions/actions';
-import { capitalize, checkFormProfile, checkPassword } from '../../utils/functions';
-import PassForm from '../PassForm/PassForm';
-import UploadImg from '../UploadImg/UploadImg';
+  Box,
+  Button,
+  FilledInput,
+  FormControl,
+  IconButton,
+  InputAdornment,
+  InputLabel,
+  TextField,
+} from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { editUser } from "../../redux/actions/actions";
+import {
+  capitalize,
+  checkFormProfile,
+  checkPassword,
+} from "../../utils/functions";
+import PassForm from "../PassForm/PassForm";
+import UploadImg from "../UploadImg/UploadImg";
+import swal from "sweetalert";
 
 const initForm = {
-    name: '',
-    lastname: '',
-    address: '',
+  name: "",
+  lastname: "",
+  address: "",
 };
 
 const initErrors = {
-    state: false,
-    name: '',
-    lastname: '',
-    address: '',
+  state: false,
+  name: "",
+  lastname: "",
+  address: "",
 };
 
 const FormProfile = () => {
-    const dispatch = useDispatch();
-    const user = useSelector((state) => state.userReducer.user);
-    const [form, setForm] = useState(initForm);
-    const [errors, setErrors] = useState(initErrors);
-    const [edit, setEdit] = useState(false);
-    const token = localStorage.getItem('token');
-    const id = localStorage.getItem('uid');
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.userReducer.user);
+  const [form, setForm] = useState(initForm);
+  const [errors, setErrors] = useState(initErrors);
+  const [edit, setEdit] = useState(false);
+  const token = localStorage.getItem("token");
+  const id = localStorage.getItem("uid");
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        if (!edit) {
-            setEdit(!edit); //ACA SE EDITA
-        } else {
-            const check = checkFormProfile(form);
-            setErrors((prevState) => {
-                return { ...prevState, ...check };
-            });
+  const userCreated = () => {
+    return swal({
+      title: "User created!",
+      text: "You can now log in",
+      icon: "success",
+      button: "OK!",
+    });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!edit) {
+      userCreated();
+      setEdit(!edit); //ACA SE EDITA
+    } else {
+      const check = checkFormProfile(form);
+      setErrors((prevState) => {
+        return { ...prevState, ...check };
+      });
 
-            if (!check.state) {
-                setEdit(!edit); //ACA SE SUBMITEA
-                dispatch(editUser(token, id, form));
-            }
-        }
-    };
+      if (!check.state) {
+        setEdit(!edit); //ACA SE SUBMITEA
+        dispatch(editUser(token, id, form));
+      }
+    }
+  };
 
-    const handleChange = (e) => {
-        setForm({
-            ...form,
-            [e.target.name]: e.target.value,
-        });
-        setErrors({
-            ...errors,
-            [e.target.name]: '',
-        });
-    };
+  const handleChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+    setErrors({
+      ...errors,
+      [e.target.name]: "",
+    });
+  };
 
-    const handleCancel = (e) => {
-        setForm({
-            name: capitalize(user.name) || '',
-            lastname: capitalize(user.lastname) || '',
-            address: capitalize(user.address) || '',
-            img: user.img || '',
-        });
-        setEdit(false);
-    };
+  const handleCancel = (e) => {
+    setForm({
+      name: capitalize(user.name) || "",
+      lastname: capitalize(user.lastname) || "",
+      address: capitalize(user.address) || "",
+      img: user.img || "",
+    });
+    setEdit(false);
+  };
 
-    useEffect(() => {
-        setForm({
-            name: capitalize(user.name) || '',
-            lastname: capitalize(user.lastname) || '',
-            address: capitalize(user.address) || '',
-            img: user.img || '',
-        });
-    }, [user]);
+  useEffect(() => {
+    setForm({
+      name: capitalize(user.name) || "",
+      lastname: capitalize(user.lastname) || "",
+      address: capitalize(user.address) || "",
+      img: user.img || "",
+    });
+  }, [user]);
 
-    return (
-        <Box>
-            <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-evenly', alignItems: 'center' }}>
-                <Box component={'form'} onSubmit={handleSubmit}>
-                    <Box
-                        sx={{
-                            height: '60vh',
-                            display: 'flex',
-                            flexDirection: 'column',
-                            justifyContent: 'space-evenly',
-                            alignItems: 'flex-start',
-                        }}
-                    >
-                        <UploadImg />
-                        <TextField
-                            id='Name'
-                            name='name'
-                            disabled={!!user.name || !edit}
-                            error={!!errors.name}
-                            helperText={!!errors.name && errors.name}
-                            variant='filled'
-                            label='Name'
-                            value={form.name}
-                            onChange={handleChange}
-                        />
-                        <TextField
-                            id='Lastname'
-                            name='lastname'
-                            disabled={!!user.lastname || !edit}
-                            error={!!errors.lastname}
-                            helperText={!!errors.lastname && errors.lastname}
-                            variant='filled'
-                            label='Lastname'
-                            value={form.lastname}
-                            onChange={handleChange}
-                        />
-                        <TextField
-                            id='Address'
-                            disabled={!edit}
-                            name='address'
-                            error={!!errors.address}
-                            helperText={!!errors.address && errors.address}
-                            variant='filled'
-                            label='Address'
-                            value={form.address}
-                            onChange={handleChange}
-                        />
-                    </Box>
+  return (
+    <Box>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-evenly",
+          alignItems: "center",
+        }}
+      >
+        <Box component={"form"} onSubmit={handleSubmit}>
+          <Box
+            sx={{
+              height: "60vh",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "space-evenly",
+              alignItems: "flex-start",
+            }}
+          >
+            <UploadImg />
+            <TextField
+              id="Name"
+              name="name"
+              disabled={!!user.name || !edit}
+              error={!!errors.name}
+              helperText={!!errors.name && errors.name}
+              variant="filled"
+              label="Name"
+              value={form.name}
+              onChange={handleChange}
+            />
+            <TextField
+              id="Lastname"
+              name="lastname"
+              disabled={!!user.lastname || !edit}
+              error={!!errors.lastname}
+              helperText={!!errors.lastname && errors.lastname}
+              variant="filled"
+              label="Lastname"
+              value={form.lastname}
+              onChange={handleChange}
+            />
+            <TextField
+              id="Address"
+              disabled={!edit}
+              name="address"
+              error={!!errors.address}
+              helperText={!!errors.address && errors.address}
+              variant="filled"
+              label="Address"
+              value={form.address}
+              onChange={handleChange}
+            />
+          </Box>
 
-                    {!edit ? (
-                        <Box>
-                            <Button variant='contained' type='submit'>
-                                Edit Profile
-                            </Button>
-                        </Box>
-                    ) : (
-                        <Box
-                            sx={{
-                                width: '15vw',
-                                display: 'flex',
-                                flexDirection: 'row',
-                                justifyContent: 'space-evenly',
-                                alignItems: 'flex-start',
-                            }}
-                        >
-                            <Button variant='contained' type='submit'>
-                                Save
-                            </Button>
-                            <Button variant='contained' color='error' onClick={handleCancel}>
-                                Cancel
-                            </Button>
-                        </Box>
-                    )}
-                </Box>
-                <img src={user?.img} alt='' width={'100px'} sx={{ alignSelf: 'flex-start' }} />
+          {!edit ? (
+            <Box>
+              <Button variant="contained" type="submit">
+                Edit Profile
+              </Button>
             </Box>
-            <PassForm />
+          ) : (
+            <Box
+              sx={{
+                width: "15vw",
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-evenly",
+                alignItems: "flex-start",
+              }}
+            >
+              <Button variant="contained" type="submit">
+                Save
+              </Button>
+              <Button variant="contained" color="error" onClick={handleCancel}>
+                Cancel
+              </Button>
+            </Box>
+          )}
         </Box>
-    );
+        <img
+          src={user?.img}
+          alt=""
+          width={"100px"}
+          sx={{ alignSelf: "flex-start" }}
+        />
+      </Box>
+      <PassForm />
+    </Box>
+  );
 };
 
 export default FormProfile;
