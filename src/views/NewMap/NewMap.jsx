5df481a0d76e426aad1ad7a615eb17ed2bb6be86
mariano-bm <mariano.bustos.moron@gmail.com>
2022-04-24 11:19:11 -0700

    .

--- src/views/NewMap/NewMap.jsx
+++ src/views/NewMap/NewMap.jsx
@@ -1,74 +1,74 @@
-import { Box } from '@mui/material';
-import { useEffect, useState } from 'react';
-import { MapContainer, Marker, TileLayer, Popup } from 'react-leaflet';
-import { useDispatch, useSelector } from 'react-redux';
-import { useLocation } from 'react-router-dom';
-import { getCaretakers } from '../../redux/actions/actions';
-import PopUpData from '../Map/PopUpData';
+import { Box } from "@mui/material";
+import { useEffect, useState } from "react";
+import { MapContainer, Marker, TileLayer, Popup } from "react-leaflet";
+import { useDispatch, useSelector } from "react-redux";
+import { useLocation } from "react-router-dom";
+import { getCaretakers } from "../../redux/actions/actions";
+import PopUpData from "../Map/PopUpData";
 
 const initialPoint = {
-	lng: -58.381592,
-	lat: -34.603722,
-	zoom: 5,
+  lng: -58.381592,
+  lat: -34.603722,
+  zoom: 5,
 };
 
 export const NewMap = () => {
-	const [myPoint, setMyPoint] = useState(initialPoint);
-	const location = useLocation();
-	const dispatch = useDispatch();
+  const [myPoint, setMyPoint] = useState(initialPoint);
+  const location = useLocation();
+  const dispatch = useDispatch();
 
-	// const [myCoordsInForm, setMyCoordsInForm] = useState(null);
-	// const dispatch = useDispatch();
-	const { filteredCaretakers } = useSelector(
-		(state) => state.cuidadoresReducer
-	);
-	// useEffect(() => {
-	// 	navigator.geolocation.getCurrentPosition((pos) => {
-	// 		const crd = pos.coords;
-	// 		if (crd.latitude) {
-	// 			setMyPoint({
-	// 				longitude: crd.longitude,
-	// 				latitude: crd.latitude,
-	// 				zoom: 12,
-	// 			});
-	// 		}
-	// 	});
-	// }, []);
+  // const [myCoordsInForm, setMyCoordsInForm] = useState(null);
+  // const dispatch = useDispatch();
+  const { filteredCaretakers } = useSelector(
+    (state) => state.cuidadoresReducer
+  );
+  // useEffect(() => {
+  // 	navigator.geolocation.getCurrentPosition((pos) => {
+  // 		const crd = pos.coords;
+  // 		if (crd.latitude) {
+  // 			setMyPoint({
+  // 				longitude: crd.longitude,
+  // 				latitude: crd.latitude,
+  // 				zoom: 12,
+  // 			});
+  // 		}
+  // 	});
+  // }, []);
 
-	useEffect(() => {
-		if (!filteredCaretakers.length) {
-			dispatch(getCaretakers());
-		}
-	}, [filteredCaretakers]);
-	return (
-		<div style={{ height: 'calc(100vh - 64px)' }}>
-			<MapContainer
-				center={[myPoint.lat, myPoint.lng]}
-				zoom={myPoint.zoom}
-				scrollWheelZoom={true}
-				style={{ height: '100%' }}
-				minZoom={3}
-			>
-				<TileLayer
-					attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-					url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-					noWrap={true}
-				/>
-				{filteredCaretakers.map((cuidador, index) => (
-					<Marker
-						key={`marker-${index}`}
-						position={[cuidador.lat, cuidador.lng]}
-						// latitude={cuidador.lat}
-						anchor='center'
-						// icon={<LocationOnIcon />}
-						color='#F29279'
-					>
-						<Popup>
-							<PopUpData person={cuidador} />
-						</Popup>
-					</Marker>
-				))}
-			</MapContainer>
-		</div>
-	);
+  useEffect(() => {
+    if (!filteredCaretakers.length) {
+      dispatch(getCaretakers());
+    }
+  }, [dispatch]);
+  return (
+    <div style={{ height: "calc(100vh - 64px)" }}>
+      <MapContainer
+        center={[myPoint.lat, myPoint.lng]}
+        zoom={myPoint.zoom}
+        scrollWheelZoom={true}
+        style={{ height: "100%" }}
+        minZoom={3}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          noWrap={true}
+        />
+        {filteredCaretakers.map((cuidador, index) => (
+          <Marker
+            key={`marker-${index}`}
+            position={[cuidador.lat, cuidador.lng]}
+            // latitude={cuidador.lat}
+            anchor="center"
+            // icon={<LocationOnIcon />}
+            color="#F29279"
+          >
+            <Popup>
+              <PopUpData person={cuidador} />
+            </Popup>
+          </Marker>
+        ))}
+      </MapContainer>
+    </div>
+  );
 };
