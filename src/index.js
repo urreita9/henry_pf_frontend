4045afc05a9bf0e935d9ca9cc51a5dde33401fe0
Mariano <mariano.bustos.moron@gmail.com>
2022-04-25 19:33:02 -0300

    1

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import App from './App';
-import store from './redux/store/store';
-import './index.css'
-import "@glidejs/glide/dist/css/glide.core.css"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import App from "./App";
+import store from "./redux/store/store";
+import "./index.css";
+import "@glidejs/glide/dist/css/glide.core.css";
 
 // ReactDOM.render(
 // 	<Provider store={store}>
@@ -20,15 +20,15 @@ import "@glidejs/glide/dist/css/glide.core.css"
 // );
 
 //
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-	<Provider store={store}>
-		{/* <Auth0Provider
+  <Provider store={store}>
+    {/* <Auth0Provider
 			domain='dev-oe7eehce.us.auth0.com'
 			clientId='XlmeWbwAL1Y0aSVChDnZlOL6XtzYYv63'
 			redirectUri={window.location.origin}
 		> */}
-		<App />
-		{/* </Auth0Provider> */}
-	</Provider>
+    <App />
+    {/* </Auth0Provider> */}
+  </Provider>
 );
