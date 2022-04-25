dab6052e514afb47c768073dfc312dddc0fda810
Mariano <mariano.bustos.moron@gmail.com>
2022-04-25 19:34:04 -0300

    2

--- src/components/Operation/Operation.jsx
+++ src/components/Operation/Operation.jsx
@@ -1,30 +1,26 @@
-import { Box, Card, Grid } from '@mui/material';
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { captureOperation } from '../../redux/actions/operationActions';
-import { useParams, useSearchParams } from 'react-router-dom';
+import { Box, Card, Grid } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { captureOperation } from "../../redux/actions/operationActions";
 
 export const Operation = () => {
   const dispatch = useDispatch();
-  const [searchParams] = useSearchParams();
-  const token = searchParams.get('token');
-  console.log(token);
-
-  const { operation, createdOperation } = useSelector(
-    (state) => state.operationsReducer
-  );
+  const { operation } = useSelector((state) => state.operationsReducer);
   console.log(operation);
-  console.log(createdOperation);
-  const { id, status } = operation;
-  // const { email_address, name: given_name, name: surname } = operation.payer;
-  useEffect(() => {
-    dispatch(captureOperation(token));
-  }, [dispatch]);
+  // const { id, userId, caretakerId, status, totalCheckout, price, timeLapse } =
+  //   operation;
+  // useEffect(() => {
+  //   // dispatch(captureOperation());
+  // }, [dispatch]);
+
+  const [data, setData] = useState("");
+
+  useEffect(() => {});
 
   return (
     <Grid item xs={12}>
       <Card>
-        <Box>Operation # {id}</Box>
+        {/* <Box>Operation # {id}</Box> */}
         {/* <Box>user # {userId}</Box> */}
         {/* <Box>caretaker # {caretakerId}</Box> */}
         {/* <Box>User Name </Box>
