ce8a09a8a18210ce80b0f38921d2642b0322ac2f
Mariano <mariano.bustos.moron@gmail.com>
2022-04-25 00:32:13 -0300

    tqm mati

--- src/components/Ticket/Ticket.jsx
+++ src/components/Ticket/Ticket.jsx
@@ -1,149 +1,149 @@
-import { useState } from 'react';
-import { intervalToDuration, differenceInCalendarDays } from 'date-fns';
-
+import { useState } from "react";
+import { intervalToDuration, differenceInCalendarDays } from "date-fns";
 import {
-	Box,
-	Card,
-	CardActions,
-	CardContent,
-	Button,
-	Typography,
-} from '@mui/material';
-
-import { useDispatch, useSelector } from 'react-redux';
-
-import { useNavigate } from 'react-router-dom';
-import api from '../../axios';
-import LoginModal from '../LoginModal/LoginModal';
+  Box,
+  Card,
+  CardActions,
+  CardContent,
+  Button,
+  Typography,
+} from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import api from "../../axios";
+import LoginModal from "../LoginModal/LoginModal";
 
 const TicketCard = ({ price, datesRange }) => {
-	const timeLapse = intervalToDuration({
-		start: new Date(datesRange[0].startDate),
-		end: new Date(datesRange[0].endDate),
-	}).days;
+  const timeLapse = intervalToDuration({
+    start: new Date(datesRange[0].startDate),
+    end: new Date(datesRange[0].endDate),
+  }).days;
 
-	const time = differenceInCalendarDays(
-		new Date(datesRange[0].endDate),
-		new Date(datesRange[0].startDate)
-	);
+  const time = differenceInCalendarDays(
+    new Date(datesRange[0].endDate),
+    new Date(datesRange[0].startDate)
+  );
 
-	const [openLogin, setOpenLogin] = useState(false);
-	const { logged, user } = useSelector((state) => state.userReducer);
-	const { caretakerProfile } = useSelector((state) => state.cuidadoresReducer);
+  const [openLogin, setOpenLogin] = useState(false);
+  const { logged, user } = useSelector((state) => state.userReducer);
+  const { caretakerProfile } = useSelector((state) => state.cuidadoresReducer);
 
-	const dispatch = useDispatch();
-	const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-	const handleLoginModal = () => {
-		setOpenLogin(!openLogin);
-	};
+  const handleLoginModal = () => {
+    setOpenLogin(!openLogin);
+  };
 
-	const sum = price * time;
-	const totalCheckout = sum + sum * 0.03;
+  const sum = price * time;
+  const totalCheckout = sum + sum * 0.03;
 
-	const handleOperationSubmit = async (
-		buyerId,
-		sellerId,
-		price,
-		datesRange,
-		timeLapse
-	) => {
-		const response = await api.post('/operations', {
-			buyerId,
-			sellerId,
-			price,
-			datesRange,
-			timeLapse,
-		});
+  const handleOperationSubmit = async (
+    buyerId,
+    sellerId,
+    price,
+    datesRange,
+    timeLapse,
+    totalCheckout
+  ) => {
+    const response = await api.post(
+      "http://localhost:3001/api/operations/create-order",
+      {
+        buyerId,
+        sellerId,
+        price,
+        datesRange,
+        timeLapse,
+        totalCheckout,
+      }
+    );
 
-
-		// ACA
-
-		window.location.href = response.data.response.init_point;
-	};
-	return (
-		<>
-			<CardContent>
-				<Typography
-					sx={{ fontSize: 14, textAlign: 'end' }}
-					color='text.secondary'
-					gutterBottom
-				>
-					${price} per night
-				</Typography>
-				<Typography variant='h5' component='div'>
-					Checkout
-				</Typography>
-				{time !== 0 && datesRange[0].endDate && (
-					<>
-						<Typography sx={{ mb: 1, textAlign: 'end' }} color='text.secondary'>
-							{datesRange[0].startDate.getDate()}/
-							{datesRange[0].startDate.getMonth()}/
-							{datesRange[0].startDate.getFullYear()} -
-							{datesRange[0].endDate.getDate()}/
-							{datesRange[0].endDate.getMonth()}/
-							{datesRange[0].endDate.getFullYear()}
-						</Typography>
-						<Typography sx={{ mb: 1 }} color='text.secondary'>
-							{time === 1
-								? `$${price} x ${time} night`
-								: `$${price} x ${time} nights`}
-						</Typography>
-						<Typography sx={{ mb: 1 }} color='text.secondary'>
-							2% Cleaning fee
-						</Typography>
-						<Typography sx={{ mb: 1 }} color='text.secondary'>
-							1% PeTrip fee
-						</Typography>
-					</>
-				)}
-				{/* <Typography sx={{ mb: 1.5 }} color='text.secondary'>
+    window.location.href = response.data.links[1].href;
+  };
+  return (
+    <>
+      <CardContent>
+        <Typography
+          sx={{ fontSize: 14, textAlign: "end" }}
+          color="text.secondary"
+          gutterBottom
+        >
+          ${price} per night
+        </Typography>
+        <Typography variant="h5" component="div">
+          Checkout
+        </Typography>
+        {time !== 0 && datesRange[0].endDate && (
+          <>
+            <Typography sx={{ mb: 1, textAlign: "end" }} color="text.secondary">
+              {datesRange[0].startDate.getDate()}/
+              {datesRange[0].startDate.getMonth()}/
+              {datesRange[0].startDate.getFullYear()} -
+              {datesRange[0].endDate.getDate()}/
+              {datesRange[0].endDate.getMonth()}/
+              {datesRange[0].endDate.getFullYear()}
+            </Typography>
+            <Typography sx={{ mb: 1 }} color="text.secondary">
+              {time === 1
+                ? `$${price} x ${time} night`
+                : `$${price} x ${time} nights`}
+            </Typography>
+            <Typography sx={{ mb: 1 }} color="text.secondary">
+              2% Cleaning fee
+            </Typography>
+            <Typography sx={{ mb: 1 }} color="text.secondary">
+              1% PeTrip fee
+            </Typography>
+          </>
+        )}
+        {/* <Typography sx={{ mb: 1.5 }} color='text.secondary'>
 					${price} x {timeLapse} nights
 				</Typography> */}
-				<Typography variant='h6' sx={{ textAlign: 'end' }}>
-					Total ${totalCheckout}
-				</Typography>
-			</CardContent>
-			<CardActions sx={{ justifyContent: 'center', alignItems: 'center' }}>
-				<Box>
-					{user?.id ? (
-						<Button
-							variant='contained'
-							size='medium'
-							sx={{ backgroundColor: '#F29278' }}
-							onClick={() => {
-								if (!logged) {
-									return;
-								}
-								handleOperationSubmit(
-									user.id,
-									caretakerProfile.id,
-									price,
-									datesRange,
-									time
-								);
-							}}
-							disabled={user.id === caretakerProfile.id ? true : false}
-						>
-							Checkout
-						</Button>
-					) : (
-						<LoginModal
-							openLogin={openLogin}
-							handleLoginModal={handleLoginModal}
-						/>
-					)}
-				</Box>
-			</CardActions>
-		</>
-	);
+        <Typography variant="h6" sx={{ textAlign: "end" }}>
+          Total ${totalCheckout}
+        </Typography>
+      </CardContent>
+      <CardActions sx={{ justifyContent: "center", alignItems: "center" }}>
+        <Box>
+          {user?.id ? (
+            <Button
+              variant="contained"
+              size="medium"
+              sx={{ backgroundColor: "#F29278" }}
+              onClick={() => {
+                if (!logged) {
+                  return;
+                }
+                handleOperationSubmit(
+                  user.id,
+                  caretakerProfile.id,
+                  price,
+                  datesRange,
+                  time,
+                  totalCheckout
+                );
+              }}
+              disabled={user.id === caretakerProfile.id ? true : false}
+            >
+              Checkout
+            </Button>
+          ) : (
+            <LoginModal
+              openLogin={openLogin}
+              handleLoginModal={handleLoginModal}
+            />
+          )}
+        </Box>
+      </CardActions>
+    </>
+  );
 };
 export const Ticket = ({ price, datesRange }) => {
-	return (
-		<Box sx={{ width: '90%' }}>
-			<Card variant='outlined'>
-				<TicketCard price={price} datesRange={datesRange} />
-			</Card>
-		</Box>
-	);
+  return (
+    <Box sx={{ width: "90%" }}>
+      <Card variant="outlined">
+        <TicketCard price={price} datesRange={datesRange} />
+      </Card>
+    </Box>
+  );
 };
--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,101 +1,131 @@
-import { Box, Container, Typography } from '@mui/material';
-import Paper from '@mui/material/Paper';
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { getCaretakers } from '../../redux/actions/actions';
-import { ButtonMapFilter } from '../../components/MapFilters/ButtonFilter';
+import { Box, Button, Container, Typography } from "@mui/material";
+import Paper from "@mui/material/Paper";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { getCaretakers } from "../../redux/actions/actions";
+import { ButtonMapFilter } from "../../components/MapFilters/ButtonFilter";
 // import StepperModal from "../Stepper/Stepper";
-import { FAQ } from '../../components/FAQ/FAQ';
-import petsHome from '../../utils/petshome.jpg';
-import personamascota from '../../utils/personaconmascota.jpg';
-import mascota2 from '../../utils/mascotas2.jpg';
+import { FAQ } from "../../components/FAQ/FAQ";
+import petsHome from "../../utils/petshome.jpg";
+import personamascota from "../../utils/personaconmascota.jpg";
+import mascota2 from "../../utils/mascotas2.jpg";
 import * as Carousel from "../../components/Carousel";
-
-
+import axios from "axios";
 // import { style } from "@mui/system";
 
+const clickHandler = async () => {
+  const response = await axios.post(
+    "http://localhost:3001/api/operations/create-order"
+  );
+  console.log(response);
+  window.location.href = response.data.links[1].href; //! importante
+};
 export const Home = () => {
-	const dispatch = useDispatch();
-	useEffect(() => {
-		dispatch(getCaretakers());
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, []);
-	return (
-		<>
-			<Container
-				sx={{
-					display: 'flex',
-					justifyContent: 'flex-end',
-					alignItems: 'center',
-					height: '80vh',
-					marginTop: '2rem',
-					backgroundColor: "background"
-				}}
-			>
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getCaretakers());
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-				<Paper
-					elevation={3}
-					sx={{
-						position: 'absolute',
-						left: '4rem',
-						zIndex: '300',
-						width: '40%',
-						maxWidth: '500px',
-						height: '70vh',
-						maxHeight: '450px',
-						display: 'flex',
-						flexDirection: 'column',
-						justifyContent: 'center',
-						alignItems: 'center',
-						borderRadius: '10px',
-						padding: '1.5rem',
-					}}
-				>
-					<Typography
-						variant='h4'
-						component='h1'
-						sx={{ textAlign: 'center', fontWeight: 'bold' }}
-					>
-						Find sitters for your pets
-					</Typography>
+  return (
+    <>
+      <Container
+        sx={{
+          display: "flex",
+          justifyContent: "flex-end",
+          alignItems: "center",
+          height: "80vh",
+          marginTop: "2rem",
+          backgroundColor: "background",
+        }}
+      >
+        <Paper
+          elevation={3}
+          sx={{
+            position: "absolute",
+            left: "4rem",
+            zIndex: "300",
+            width: "40%",
+            maxWidth: "500px",
+            height: "70vh",
+            maxHeight: "450px",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            borderRadius: "10px",
+            padding: "1.5rem",
+          }}
+        >
+          <Typography
+            variant="h4"
+            component="h1"
+            sx={{ textAlign: "center", fontWeight: "bold" }}
+          >
+            Find sitters for your pets
+          </Typography>
 
-					<ButtonMapFilter />
-				</Paper>
-				<Paper
-					elevation={2}
-					sx={{
-						// backgroundImage: `url(${petsHome})`,
-						// backgroundSize: 'cover',
-						// backgroundPosition: 'center',
-						// backgroundRepeat: 'no-repeat',
-						display: 'flex',
-						flexDirection: 'column',
-						justifyContent: 'center',
-						alignItems: 'center',
-						width: '80%',
-						// minHeight: '500px',
-						// height: '100%',
-						borderRadius: '10px',
-					}}
-				>
-					    <Carousel.Component options={{ autoplay: 2000 }}>
-							<Carousel.Slide>
-								<img src={petsHome} width='100%' height="500px" alt="" borderRadius="10px" style={{borderRadius: '10px'}} />
-							</Carousel.Slide>
-							<Carousel.Slide ><img src={personamascota} width='100%' height="500px" alt="" style={{borderRadius: '10px'}} /></Carousel.Slide>
-							<Carousel.Slide><img src={mascota2} width='100%' height="500px" alt="" style={{borderRadius: '10px'}} /></Carousel.Slide>
-						</Carousel.Component>
-					{/* <StepperModal /> */}
-					{/* <Typography variant="h2" sx={{ textAlign: "center" }}>
+          <ButtonMapFilter />
+        </Paper>
+        <Paper
+          elevation={2}
+          sx={{
+            // backgroundImage: `url(${petsHome})`,
+            // backgroundSize: 'cover',
+            // backgroundPosition: 'center',
+            // backgroundRepeat: 'no-repeat',
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            width: "80%",
+            // minHeight: '500px',
+            // height: '100%',
+            borderRadius: "10px",
+          }}
+        >
+          <Carousel.Component options={{ autoplay: 2000 }}>
+            <Carousel.Slide>
+              <img
+                src={petsHome}
+                width="100%"
+                height="500px"
+                alt=""
+                borderRadius="10px"
+                style={{ borderRadius: "10px" }}
+              />
+            </Carousel.Slide>
+            <Carousel.Slide>
+              <img
+                src={personamascota}
+                width="100%"
+                height="500px"
+                alt=""
+                style={{ borderRadius: "10px" }}
+              />
+            </Carousel.Slide>
+            <Carousel.Slide>
+              <img
+                src={mascota2}
+                width="100%"
+                height="500px"
+                alt=""
+                style={{ borderRadius: "10px" }}
+              />
+            </Carousel.Slide>
+          </Carousel.Component>
+          {/* <StepperModal /> */}
+          {/* <Typography variant="h2" sx={{ textAlign: "center" }}>
               Find the best place for your pet!
             </Typography> */}
 
-					{/* <img src={petsHome} alt="" height={'100%'}  style={{borderRadius: '5px'}}/> */}
-				</Paper>
-			</Container>
-			<Container>
-				<FAQ />
-			</Container>
-		</>
-	);
+          {/* <img src={petsHome} alt="" height={'100%'}  style={{borderRadius: '5px'}}/> */}
+        </Paper>
+      </Container>
+      <Container>
+        <FAQ />
+      </Container>
+      <Button onClick={clickHandler}> Checkout test</Button>
+    </>
+  );
 };
