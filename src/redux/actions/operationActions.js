5d511025dbd8fc9a1838a0278423a70cdd3c6ec6
matiasangelani <matiasangelani2@gmail.com>
2022-04-25 21:14:32 -0300

    prop PayerID added to captureOperation actions

--- src/redux/actions/operationActions.js
+++ src/redux/actions/operationActions.js
@@ -1,10 +1,10 @@
-import api from "../../axios";
-export const POST_OPERATION = "POST_OPERATION";
-export const GET_USER_OPERATIONS = "GET_USER_OPERATIONS";
-export const UPDATE_OP_STATUS = "UPDATE_OP_STATUS";
+import api from '../../axios';
+export const POST_OPERATION = 'POST_OPERATION';
+export const GET_USER_OPERATIONS = 'GET_USER_OPERATIONS';
+export const UPDATE_OP_STATUS = 'UPDATE_OP_STATUS';
 
-export const SET_OPERATION = "SET_OPERATION"; //MOMENTANEA
-export const CAPTURE_OPERATION = "CAPTURE_OPERATION";
+export const SET_OPERATION = 'SET_OPERATION'; //MOMENTANEA
+export const CAPTURE_OPERATION = 'CAPTURE_OPERATION';
 
 export const getUserOperations = (id, user) => async (dispatch) => {
   try {
@@ -27,7 +27,7 @@ export const updateOpStatus =
         { idOperation, idPayment },
         {
           headers: {
-            "x-token": token,
+            'x-token': token,
           },
         }
       );
@@ -53,7 +53,7 @@ export const setOperation =
   ({ id, totalCheckout, timeLapse, uid }) =>
   async (dispatch) => {
     try {
-      const { data } = await api.post("/operations/create-order", {
+      const { data } = await api.post('/operations/create-order', {
         headers: {
           uid,
         },
@@ -72,10 +72,12 @@ export const setOperation =
     }
   };
 
-export const captureOperation = (token) => async (dispatch) => {
+export const captureOperation = (token, PayerID) => async (dispatch) => {
   try {
-    const { data } = await api.get(`/operations/capture-order?token=${token}`);
-    console.log("ACTION CAPTURE", data);
+    const { data } = await api.get(
+      `/operations/capture-order?token=${token}&PayerID=${PayerID}`
+    );
+    console.log('ACTION CAPTURE', data);
     dispatch({
       type: CAPTURE_OPERATION,
       payload: data,
