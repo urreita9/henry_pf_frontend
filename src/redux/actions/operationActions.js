0af9c966c15136e2ab645b97134a179d0150b1fd
matiasangelani <matiasangelani2@gmail.com>
2022-04-28 01:41:39 -0300

    action and case clearOperations added to redux

--- src/redux/actions/operationActions.js
+++ src/redux/actions/operationActions.js
@@ -7,48 +7,49 @@ export const SET_OPERATION = 'SET_OPERATION'; //MOMENTANEA
 export const CAPTURE_OPERATION = 'CAPTURE_OPERATION';
 export const SELECT_OPERATION = 'SELECT_OPERATION';
 export const FILTER_BY_DATE = 'FILTER_BY_DATE';
+export const CLEAR_OPERATIONS = 'CLEAR_OPERATIONS';
 
 export const getUserOperations = (uid, token, user) => async (dispatch) => {
-	try {
-		const { data } = await api.get(`/operations?user=${user}`, {
-			headers: {
-				'x-token': token,
-				uid,
-			},
-		});
+  try {
+    const { data } = await api.get(`/operations?user=${user}`, {
+      headers: {
+        'x-token': token,
+        uid,
+      },
+    });
 
-		if (data.length) {
-			dispatch({
-				type: GET_USER_OPERATIONS,
-				payload: data,
-			});
-		}
-	} catch (error) {
-		alert(error);
-	}
+    //if (data.length) {
+    dispatch({
+      type: GET_USER_OPERATIONS,
+      payload: data,
+    });
+    //}
+  } catch (error) {
+    alert(error);
+  }
 };
 
 export const updateOpStatus =
-	(idOperation, idPayment, token) => async (dispatch) => {
-		try {
-			const { data } = await api.put(
-				`/operations`,
-				{ idOperation, idPayment },
-				{
-					headers: {
-						'x-token': token,
-					},
-				}
-			);
+  (idOperation, idPayment, token) => async (dispatch) => {
+    try {
+      const { data } = await api.put(
+        `/operations`,
+        { idOperation, idPayment },
+        {
+          headers: {
+            'x-token': token,
+          },
+        }
+      );
 
-			dispatch({
-				type: UPDATE_OP_STATUS,
-				payload: data,
-			});
-		} catch (error) {
-			alert(error);
-		}
-	};
+      dispatch({
+        type: UPDATE_OP_STATUS,
+        payload: data,
+      });
+    } catch (error) {
+      alert(error);
+    }
+  };
 
 // export const setOperation = (payload) => {
 //   // console.log(data);
@@ -59,50 +60,55 @@ export const updateOpStatus =
 // };
 
 export const setOperation =
-	({ id, totalCheckout, timeLapse, uid, pet }) =>
-	async (dispatch) => {
-		try {
-			const { data } = await api.post('/operations/create-order', {
-				id,
-				totalCheckout,
-				timeLapse,
-				petId: pet,
-				headers: {
-					uid,
-				},
-			});
-			dispatch({
-				type: SET_OPERATION,
-				payload: data,
-			});
+  ({ id, totalCheckout, timeLapse, uid, pet }) =>
+  async (dispatch) => {
+    try {
+      const { data } = await api.post('/operations/create-order', {
+        id,
+        totalCheckout,
+        timeLapse,
+        petId: pet,
+        headers: {
+          uid,
+        },
+      });
+      dispatch({
+        type: SET_OPERATION,
+        payload: data,
+      });
 
-			window.location.href = data.links[1].href;
-		} catch (error) {
-			alert(error);
-		}
-	};
+      window.location.href = data.links[1].href;
+    } catch (error) {
+      alert(error);
+    }
+  };
 
 export const captureOperation = (token, PayerID) => async (dispatch) => {
-	try {
-		const { data } = await api.get(
-			`/operations/capture-order?token=${token}&PayerID=${PayerID}`
-		);
-		console.log('ACTION CAPTURE', data);
-		dispatch({
-			type: CAPTURE_OPERATION,
-			payload: data,
-		});
-	} catch (error) {
-		alert(error);
-	}
+  try {
+    const { data } = await api.get(
+      `/operations/capture-order?token=${token}&PayerID=${PayerID}`
+    );
+    console.log('ACTION CAPTURE', data);
+    dispatch({
+      type: CAPTURE_OPERATION,
+      payload: data,
+    });
+  } catch (error) {
+    alert(error);
+  }
 };
 
 export const selectOperation = (id, user) => ({
-	type: SELECT_OPERATION,
-	payload: { id, user },
+  type: SELECT_OPERATION,
+  payload: { id, user },
 });
 
 export const filterByDate = (days) => ({
-	type: FILTER_BY_DATE,
-	payload: days,
+  type: FILTER_BY_DATE,
+  payload: days,
+});
+
+export const clearOperations = () => ({
+  type: CLEAR_OPERATIONS,
+  payload: [],
 });
--- src/redux/reducers/operationsReducer.js
+++ src/redux/reducers/operationsReducer.js
@@ -1,73 +1,80 @@
 import { differenceInCalendarDays } from 'date-fns';
 import {
-	SET_OPERATION,
-	GET_USER_OPERATIONS,
-	UPDATE_OP_STATUS,
-	CAPTURE_OPERATION,
-	SELECT_OPERATION,
-	FILTER_BY_DATE,
+  SET_OPERATION,
+  GET_USER_OPERATIONS,
+  UPDATE_OP_STATUS,
+  CAPTURE_OPERATION,
+  SELECT_OPERATION,
+  FILTER_BY_DATE,
+  CLEAR_OPERATIONS,
 } from '../actions/operationActions';
 
 const initialState = {
-	operations: [],
-	filteredOperations: [],
-	createdOperation: {},
-	operation: {},
+  operations: [],
+  filteredOperations: [],
+  createdOperation: {},
+  operation: {},
 };
 
 const operationsReducer = (state = initialState, action) => {
-	const { type, payload } = action;
-	switch (type) {
-		case GET_USER_OPERATIONS:
-			return {
-				...state,
-				operations: payload,
-				filteredOperations: payload,
-			};
-		case SET_OPERATION:
-			return {
-				...state,
-				createdOperation: payload,
-			};
-		case UPDATE_OP_STATUS:
-			return {
-				...state,
-				operation: payload,
-			};
-		case CAPTURE_OPERATION:
-			return {
-				...state,
-				operation: payload,
-			};
-		case SELECT_OPERATION:
-			const myOperation = state.operations.find(
-				(op) => op.operation.id === payload.id
-			);
+  const { type, payload } = action;
+  switch (type) {
+    case GET_USER_OPERATIONS:
+      return {
+        ...state,
+        operations: payload,
+        filteredOperations: payload,
+      };
+    case SET_OPERATION:
+      return {
+        ...state,
+        createdOperation: payload,
+      };
+    case UPDATE_OP_STATUS:
+      return {
+        ...state,
+        operation: payload,
+      };
+    case CAPTURE_OPERATION:
+      return {
+        ...state,
+        operation: payload,
+      };
+    case SELECT_OPERATION:
+      const myOperation = state.operations.find(
+        (op) => op.operation.id === payload.id
+      );
 
-			return {
-				...state,
-				operation: { ...myOperation, user: payload.user },
-			};
-		case FILTER_BY_DATE:
-			if (payload === 0) {
-				return {
-					...state,
-					filteredOperations: state.operations,
-				};
-			}
-			const filterByDate = state.operations.filter((op) => {
-				const time = differenceInCalendarDays(
-					new Date(),
-					new Date(op.operation.createdAt)
-				);
-				if (time <= payload) {
-					return op;
-				}
-			});
-			return { ...state, filteredOperations: filterByDate };
-		default:
-			return state;
-	}
+      return {
+        ...state,
+        operation: { ...myOperation, user: payload.user },
+      };
+    case FILTER_BY_DATE:
+      if (payload === 0) {
+        return {
+          ...state,
+          filteredOperations: state.operations,
+        };
+      }
+      const filterByDate = state.operations.filter((op) => {
+        const time = differenceInCalendarDays(
+          new Date(),
+          new Date(op.operation.createdAt)
+        );
+        if (time <= payload) {
+          return op;
+        }
+      });
+      return { ...state, filteredOperations: filterByDate };
+    case CLEAR_OPERATIONS:
+      return {
+        ...state,
+        operations: payload,
+        filteredOperations: payload,
+      };
+    default:
+      return state;
+  }
 };
 
 export default operationsReducer;
