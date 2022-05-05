ed2c09102712a791bccfaf8667e830fd86f6f803
cicarelligiuliano <cicarelligiuliano@outlook.com.ar>
2022-05-05 03:17:36 -0300

    Final

--- src/components/HistoryTable/HistoryTable.jsx
+++ src/components/HistoryTable/HistoryTable.jsx
@@ -10,114 +10,104 @@ import { capitalize } from '../../utils/functions';
 import { Avatar, Box, Button, Typography } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import {
-	finishOperation,
-	selectOperation,
-} from '../../redux/actions/operationActions';
+import { finishOperation, selectOperation } from '../../redux/actions/operationActions';
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
-	[`&.${tableCellClasses.head}`]: {
-		backgroundColor: theme.palette.primary.main,
-		color: theme.palette.text.hint,
-	},
-	[`&.${tableCellClasses.body}`]: {
-		fontSize: 14,
-	},
+    [`&.${tableCellClasses.head}`]: {
+        backgroundColor: theme.palette.primary.main,
+        color: theme.palette.text.hint,
+    },
+    [`&.${tableCellClasses.body}`]: {
+        fontSize: 14,
+    },
 }));
 
 const StyledTableRow = styled(TableRow)(({ theme }) => ({
-	'&:nth-of-type(odd)': {
-		backgroundColor: theme.palette.background.paper,
-	},
-	// hide last border
-	'&:last-child td, &:last-child th': {
-		border: 0,
-	},
+    '&:nth-of-type(odd)': {
+        backgroundColor: theme.palette.background.paper,
+    },
+    // hide last border
+    '&:last-child td, &:last-child th': {
+        border: 0,
+    },
 }));
 
 export default function CustomizedTables({ filteredOperations }) {
-	const { user } = useSelector((state) => state.userReducer);
-	const dispatch = useDispatch();
-	const navigate = useNavigate();
-	const uid = localStorage.getItem('uid');
+    const { user } = useSelector((state) => state.userReducer);
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const uid = localStorage.getItem('uid');
 
-	console.log('TABLE', filteredOperations);
-	return (
-		<>
-			{filteredOperations?.length && (
-				<>
-					{' '}
-					<TableContainer
-						component={Paper}
-						sx={{ maxWidth: 800, margin: '20px auto' }}
-					>
-						<Table sx={{ minWidth: 400 }} aria-label='customized table'>
-							<TableHead>
-								<TableRow>
-									<StyledTableCell align='left'>Caretaker</StyledTableCell>
-									<StyledTableCell align='center'>Nights</StyledTableCell>
-									<StyledTableCell align='center'>Total</StyledTableCell>
-									<StyledTableCell align='center'>Date</StyledTableCell>
-									<StyledTableCell align='center'>Pet</StyledTableCell>
-									<StyledTableCell align='center'>Status</StyledTableCell>
-									<StyledTableCell align='center'>Actions</StyledTableCell>
-								</TableRow>
-							</TableHead>
-							<TableBody>
-								{filteredOperations?.map((operation) => (
-									<StyledTableRow key={operation.operation.id}>
-										<StyledTableCell component='th' scope='row'>
-											<Box style={{ display: 'flex', alignItems: 'center' }}>
-												{' '}
-												<Avatar src={operation.caretaker.img} />
-												<Typography sx={{ marginLeft: '5px' }}>
-													{capitalize(operation.caretaker.name)}{' '}
-													{capitalize(operation.caretaker.lastname)}
-												</Typography>
-											</Box>
-										</StyledTableCell>
-										<StyledTableCell align='center'>
-											{operation.operation.timeLapse}
-										</StyledTableCell>
-										<StyledTableCell align='center'>
-											${operation.operation.price}
-										</StyledTableCell>
-										<StyledTableCell align='right'>
-											{new Date(operation.operation.createdAt).getDate()}/
-											{new Date(operation.operation.createdAt).getMonth() + 1}/
-											{new Date(operation.operation.createdAt).getFullYear()}
-										</StyledTableCell>
-										<StyledTableCell component='th' scope='row'>
-											<Box style={{ display: 'flex', alignItems: 'center' }}>
-												{' '}
-												<Avatar src={operation.pet.img} />
-												<Typography sx={{ marginLeft: '5px' }}>
-													{capitalize(operation.pet.name)}{' '}
-												</Typography>
-											</Box>
-										</StyledTableCell>
-										<StyledTableCell align='right'>
-											<Typography>{operation.operation.status}</Typography>
-										</StyledTableCell>
-										<StyledTableCell align='right'>
-											<Button
-												onClick={() => {
-													navigate(`/caretaker/${operation.caretaker.id}`);
-												}}
-											>
-												See Profile
-											</Button>
-											<Button
-												onClick={() => {
-													dispatch(
-														selectOperation(operation.operation.id, user)
-													);
-													navigate(`/operation/${operation.operation.id}`);
-												}}
-											>
-												Operation Detail
-											</Button>
-											{!operation.operation.petReceived && (
+    console.log('TABLE', filteredOperations);
+    return (
+        <>
+            {filteredOperations?.length && (
+                <>
+                    {' '}
+                    <TableContainer component={Paper} sx={{ maxWidth: 800, margin: '20px auto' }}>
+                        <Table sx={{ minWidth: 400 }} aria-label='customized table'>
+                            <TableHead>
+                                <TableRow>
+                                    <StyledTableCell align='left'>Caretaker</StyledTableCell>
+                                    <StyledTableCell align='center'>Nights</StyledTableCell>
+                                    <StyledTableCell align='center'>Total</StyledTableCell>
+                                    <StyledTableCell align='center'>Date</StyledTableCell>
+                                    <StyledTableCell align='center'>Pet</StyledTableCell>
+                                    <StyledTableCell align='center'>Status</StyledTableCell>
+                                    <StyledTableCell align='center'>Actions</StyledTableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {filteredOperations?.map((operation) => (
+                                    <StyledTableRow key={operation.operation.id}>
+                                        <StyledTableCell component='th' scope='row'>
+                                            <Box style={{ display: 'flex', alignItems: 'center' }}>
+                                                {' '}
+                                                <Avatar src={operation.caretaker.img} />
+                                                <Typography sx={{ marginLeft: '5px' }}>
+                                                    {capitalize(operation.caretaker.name)}{' '}
+                                                    {capitalize(operation.caretaker.lastname)}
+                                                </Typography>
+                                            </Box>
+                                        </StyledTableCell>
+                                        <StyledTableCell align='center'>
+                                            {operation.operation.timeLapse}
+                                        </StyledTableCell>
+                                        <StyledTableCell align='center'>${operation.operation.price}</StyledTableCell>
+                                        <StyledTableCell align='right'>
+                                            {new Date(operation.operation.createdAt).getDate()}/
+                                            {new Date(operation.operation.createdAt).getMonth() + 1}/
+                                            {new Date(operation.operation.createdAt).getFullYear()}
+                                        </StyledTableCell>
+                                        <StyledTableCell component='th' scope='row'>
+                                            <Box style={{ display: 'flex', alignItems: 'center' }}>
+                                                {' '}
+                                                <Avatar src={operation.pet.img} />
+                                                <Typography sx={{ marginLeft: '5px' }}>
+                                                    {capitalize(operation.pet.name)}{' '}
+                                                </Typography>
+                                            </Box>
+                                        </StyledTableCell>
+                                        <StyledTableCell align='right'>
+                                            <Typography>{operation.operation.status}</Typography>
+                                        </StyledTableCell>
+                                        <StyledTableCell align='right'>
+                                            <Button
+                                                onClick={() => {
+                                                    navigate(`/caretaker/${operation.caretaker.id}`);
+                                                }}
+                                            >
+                                                See Profile
+                                            </Button>
+                                            <Button
+                                                onClick={() => {
+                                                    dispatch(selectOperation(operation.operation.id, user));
+                                                    navigate(`/operation/${operation.operation.id}`);
+                                                }}
+                                            >
+                                                Operation Detail
+                                            </Button>
+                                            {/* {!operation.operation.petReceived && (
 												<Button
 													onClick={() => {
 														dispatch(
@@ -127,15 +117,15 @@ export default function CustomizedTables({ filteredOperations }) {
 												>
 													I Received my pet
 												</Button>
-											)}
-										</StyledTableCell>
-									</StyledTableRow>
-								))}
-							</TableBody>
-						</Table>
-					</TableContainer>
-				</>
-			)}
-		</>
-	);
+											)} */}
+                                        </StyledTableCell>
+                                    </StyledTableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                </>
+            )}
+        </>
+    );
 }
--- src/redux/reducers/operationsReducer.js
+++ src/redux/reducers/operationsReducer.js
@@ -1,118 +1,109 @@
 import { differenceInCalendarDays } from 'date-fns';
 import {
-	SET_OPERATION,
-	GET_USER_OPERATIONS,
-	UPDATE_OP_STATUS,
-	CAPTURE_OPERATION,
-	SELECT_OPERATION,
-	FILTER_BY_DATE,
-	CLEAR_OPERATIONS,
-	GET_ALL_OPERATIONS,
-	GET_CARETAKER_OPERATIONS,
-	FILTER_BY_DATE_CARETAKER,
+    SET_OPERATION,
+    GET_USER_OPERATIONS,
+    UPDATE_OP_STATUS,
+    CAPTURE_OPERATION,
+    SELECT_OPERATION,
+    FILTER_BY_DATE,
+    CLEAR_OPERATIONS,
+    GET_ALL_OPERATIONS,
+    GET_CARETAKER_OPERATIONS,
+    FILTER_BY_DATE_CARETAKER,
 } from '../actions/operationActions';
 
 const initialState = {
-	operations: [],
-	filteredOperations: [],
-	caretakerOperations: [],
-	createdOperation: {},
-	operation: {},
-	//allOperationsAdmins: []
+    operations: [],
+    filteredOperations: [],
+    caretakerOperations: [],
+    createdOperation: {},
+    operation: {},
+    //allOperationsAdmins: []
 };
 
 const operationsReducer = (state = initialState, action) => {
-	const { type, payload } = action;
-	switch (type) {
-		case GET_USER_OPERATIONS:
-			return {
-				...state,
-				operations: payload,
-				filteredOperations: payload,
-			};
-		case SET_OPERATION:
-			return {
-				...state,
-				createdOperation: payload,
-			};
-		case UPDATE_OP_STATUS:
-			return {
-				...state,
-				operations: payload,
-			};
-		case CAPTURE_OPERATION:
-			return {
-				...state,
-				operation: payload,
-			};
-		case SELECT_OPERATION:
-			let myOperation;
-			if (!payload.isCaretaker) {
-				myOperation = state.caretakerOperations.find(
-					(op) => op.operation.id === payload.id
-				);
-			} else {
-				myOperation = state.operations.find(
-					(op) => op.operation.id === payload.id
-				);
-			}
+    const { type, payload } = action;
+    switch (type) {
+        case GET_USER_OPERATIONS:
+            return {
+                ...state,
+                operations: payload,
+                filteredOperations: payload,
+                caretakerOperations: payload,
+            };
+        case SET_OPERATION:
+            return {
+                ...state,
+                createdOperation: payload,
+            };
+        case UPDATE_OP_STATUS:
+            return {
+                ...state,
+                operations: payload,
+            };
+        case CAPTURE_OPERATION:
+            return {
+                ...state,
+                operation: payload,
+            };
+        case SELECT_OPERATION:
+            let myOperation;
+            if (!payload.isCaretaker) {
+                myOperation = state.caretakerOperations.find((op) => op.operation.id === payload.id);
+            } else {
+                myOperation = state.operations.find((op) => op.operation.id === payload.id);
+            }
 
-			return {
-				...state,
-				operation: { ...myOperation, user: payload.user },
-			};
-		case FILTER_BY_DATE:
-			if (payload === 0) {
-				return {
-					...state,
-					filteredOperations: state.operations,
-				};
-			}
-			const filterByDate = state.operations.filter((op) => {
-				const time = differenceInCalendarDays(
-					new Date(),
-					new Date(op.operation.createdAt)
-				);
-				if (time <= payload) {
-					return op;
-				}
-			});
-			return { ...state, filteredOperations: filterByDate };
+            return {
+                ...state,
+                operation: { ...myOperation, user: payload.user },
+            };
+        case FILTER_BY_DATE:
+            if (payload === 0) {
+                return {
+                    ...state,
+                    filteredOperations: state.operations,
+                };
+            }
+            const filterByDate = state.operations.filter((op) => {
+                const time = differenceInCalendarDays(new Date(), new Date(op.operation.createdAt));
+                if (time <= payload) {
+                    return op;
+                }
+            });
+            return { ...state, filteredOperations: filterByDate };
 
-		case FILTER_BY_DATE_CARETAKER:
-			if (payload === 0) {
-				return {
-					...state,
-					filteredOperations: state.operations,
-				};
-			}
-			const filterByDateCaretaker = state.caretakerOperations.filter((op) => {
-				const time = differenceInCalendarDays(
-					new Date(),
-					new Date(op.operation.createdAt)
-				);
-				if (time <= payload) {
-					return op;
-				}
-			});
-			return { ...state, caretakerOperations: filterByDateCaretaker };
-		case CLEAR_OPERATIONS:
-			return {
-				...state,
-				operations: payload,
-				filteredOperations: payload,
-			};
-		case GET_ALL_OPERATIONS:
-			return {
-				...state,
-				//allOperationsAdmins: payload,
-				operations: payload,
-			};
-		case GET_CARETAKER_OPERATIONS:
-			return { ...state, caretakerOperations: [...payload] };
-		default:
-			return state;
-	}
+        case FILTER_BY_DATE_CARETAKER:
+            if (payload === 0) {
+                return {
+                    ...state,
+                    filteredOperations: state.operations,
+                };
+            }
+            const filterByDateCaretaker = state.caretakerOperations.filter((op) => {
+                const time = differenceInCalendarDays(new Date(), new Date(op.operation.createdAt));
+                if (time <= payload) {
+                    return op;
+                }
+            });
+            return { ...state, caretakerOperations: filterByDateCaretaker };
+        case CLEAR_OPERATIONS:
+            return {
+                ...state,
+                operations: payload,
+                filteredOperations: payload,
+            };
+        case GET_ALL_OPERATIONS:
+            return {
+                ...state,
+                //allOperationsAdmins: payload,
+                operations: payload,
+            };
+        case GET_CARETAKER_OPERATIONS:
+            return { ...state, caretakerOperations: payload };
+        default:
+            return state;
+    }
 };
 
 export default operationsReducer;
